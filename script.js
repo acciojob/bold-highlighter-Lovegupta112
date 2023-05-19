2cc0bd40876149914b634205ceca700e6168338c
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-19 11:16:03 +0530

    added logic for js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+let strong=document.getElementsByTagName('strong');
+console.log(strong);
 function highlight() {
     //Write your code here
-
-
+for(let elm of strong){
+    // console.log(elm);
+    elm.style.color='green';
+}
 }
 
 
 function return_normal() {
     //Write your code here
 
-    
+    for(let elm of strong){
+        elm.style.color='black';
+    }
 }
